583088d6c6bc7e3d7ceca64713f464919fb120b4
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-19 16:22:07 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -5,7 +5,7 @@ bool alertBreach(AlertTarget alertTarget, BreachType processedBreachType);
 void sendAlertToController(BreachType breachType);
 void sendAlertToEmail(BreachType breachType);
 BatteryParam_st classifyTemp(CoolingType coolingType);
-void test(AlertTarget alertTarget, CoolingType coolingType, double temperatureInC);
+bool test(AlertTarget alertTarget, CoolingType coolingType, double temperatureInC);
 bool validateRange(size_t var1, size_t var2);
 void PrintToController (const unsigned short header, BreachType breachType);
 void PrintToEmail (const char* recepient, char message[]);
