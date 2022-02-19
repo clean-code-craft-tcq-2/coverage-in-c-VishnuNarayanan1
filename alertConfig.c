b1d8a9c113806559c212cdb93075c7393e1824a5
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-19 13:38:35 +0530

    Update alertConfig.c

--- alertConfig.c
+++ alertConfig.c
@@ -21,22 +21,5 @@ BatteryParam_st BatteryParamValues[MAX_COOLING_TYPES] =
   }
 };
 
-BreachMailNotification_st BreachMailNotification[MAX_BREACH_TYPES]=
-{
-  {
-    .breachType    = NORMAL,
-    .alertTarget  = TO_CONTROLLER,
-  },
-  {
-    .breachType    = TOO_LOW,
-    .alertTarget  = TO_EMAIL,
-  },
-  {
-    .breachType    = TOO_HIGH,
-    .alertTarget  = TO_EMAIL,
-  }
-};
-
-
 char AlertMessageOverEmail[MAX_BREACH_TYPES][100] =
 {"Not Applicable","Hi, the temperature is too low\n","Hi, the temperature is too high\n"};
