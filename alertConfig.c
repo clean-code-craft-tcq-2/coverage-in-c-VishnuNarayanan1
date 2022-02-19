3ecbcb785112762900ad86e863ccc4be35890205
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-19 15:55:53 +0530

    Update alertConfig.c

--- alertConfig.c
+++ alertConfig.c
@@ -20,6 +20,16 @@ BatteryParam_st BatteryParamValues[MAX_COOLING_TYPES] =
   .higherLimitTemp = 40
   }
 };
-
+AlertTarget_st AlertTargetInfo[MAX_ALERT_TARGET_POSSIBILITIES] =
+{
+  {
+    .alertTarget = TO_CONTROLLER,
+    .alertTargetFunction = &sendAlertToController
+  },
+  {
+     .alertTarget = TO_EMAIL
+    .alertTargetFunction = &sendAlertToEmail
+  }
+};
 char AlertMessageOverEmail[MAX_BREACH_TYPES][100] =
 {"Not Applicable","Hi, the temperature is too low\n","Hi, the temperature is too high\n"};
