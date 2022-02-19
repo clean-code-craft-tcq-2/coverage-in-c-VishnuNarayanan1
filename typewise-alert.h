10c40ca887d459b2c8ac38040dd103ca0544e8ad
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-19 08:24:11 +0530

    Update typewise-alert.h

--- typewise-alert.h
+++ typewise-alert.h
@@ -1,8 +1,9 @@
 #pragma once
 
-BreachType inferBreach(double value, double lowerLimit, double upperLimit);
+BreachType inferBreach(double value, BatteryParam_st batteryLimits);
 BreachType classifyTemperatureBreach(CoolingType coolingType, double temperatureInC);
 void checkAndAlert(AlertTarget alertTarget, BatteryCharacter batteryChar, double temperatureInC);
 void sendToController(BreachType breachType);
 void sendToEmail(BreachType breachType);
 BatteryParam_st classifyTemp(CoolingType coolingType);
+void test(AlertTarget alertTarget, CoolingType coolingType, double temperatureInC);
