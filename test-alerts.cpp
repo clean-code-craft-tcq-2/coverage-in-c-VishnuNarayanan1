05a239f7f453230a409c388aa42586cdd66c6bd5
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-19 18:17:33 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -4,6 +4,22 @@
 #include "alertConfig.h"
 #include "typewise-alert.h"
 
+#ifdef TEST_ENVIRONMENT
+int Test_PrintToControllerStubCount;
+int Test_PrintToEmailStubCount;
+void PrintToControllerStub (const unsigned short Header, BreachType BreachTypeInfo)
+{
+      Test_PrintToControllerStubCount++;
+}
+void PrintToEmailStub (const char* Recepient, char Message[])
+{
+      Test_PrintToEmailStubCount++;
+}
+
+FnPtrPrintToController FuncPointerPrintToController = &PrintToControllerStub ;
+FnPtrPrintToEmail FuncPointerPrintToEmail = &PrintToEmailStub ;
+#endif 
+
 TEST_CASE("infers the breach according to limits") {
   REQUIRE(inferBreach(12, 20, 30) == TOO_LOW);
 }
