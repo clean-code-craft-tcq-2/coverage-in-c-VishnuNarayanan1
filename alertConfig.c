24749c2f7d187455747a122db253431d903765b0
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-19 08:07:10 +0530

    Update alertConfig.c

--- alertConfig.c
+++ alertConfig.c
@@ -1,1 +1,21 @@
+#include "alertConfig.h"
+#include <stdio.h>
 
+struct BatteryParam BatteryParamValues[MAX_COOLING_TYPES] =
+{
+  {
+  .coolingType =  PASSIVE_COOLING,
+  .lowerLimitTemp = 0,
+  .higherLimitTemp = 35
+  },
+  {
+  .coolingType =  HI_ACTIVE_COOLING,
+  .lowerLimitTemp = 0,
+  .higherLimitTemp = 45
+  },
+  {
+  .coolingType = MED_ACTIVE_COOLING,
+  .lowerLimitTemp = 0,
+  .higherLimitTemp = 40
+  }
+};
