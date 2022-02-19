f513ee00ff6263bd95f07304c57287364b5cd39a
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-19 15:55:58 +0530

    Update alertConfig.h

--- alertConfig.h
+++ alertConfig.h
@@ -1,6 +1,5 @@
 #include <stddef.h>
 #define MAX_COOLING_TYPES   3
-#define MAX_BREACH_TYPES    3
 #define INVALID_TEMPERATURE 0xff
 #define SUCCESS true
 #define FAILURE false
@@ -16,11 +15,13 @@ typedef enum {
   NORMAL,
   TOO_LOW,
   TOO_HIGH,
+  MAX_BREACH_TYPES
 } BreachType;
 
 typedef enum {
   TO_CONTROLLER,
-  TO_EMAIL
+  TO_EMAIL,
+  MAX_ALERT_TARGET_POSSIBILITIES
 } AlertTarget;
 
 typedef struct {
@@ -29,6 +30,14 @@ typedef struct {
   double higherLimitTemp;
 } BatteryParam_st;
 
+typedef void (*FnPtrAlertTarget)(BreachType);
 
+typedef struct 
+{
+  AlertTarget alertTarget;
+  FnPtrAlertTarget alertTargetFunction;
+}AlertTarget_st;
+
+extern AlertTarget_st AlertTargetInfo[MAX_ALERT_TARGET_POSSIBILITIES];
 extern BatteryParam_st BatteryParamValues[MAX_COOLING_TYPES];
 extern char AlertMessageOverEmail[MAX_BREACH_TYPES][100];
