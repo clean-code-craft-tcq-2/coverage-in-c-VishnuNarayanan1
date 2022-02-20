298823fca499d0a0604c54bbf3992dcea7c2f120
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-20 17:02:57 +0530

    Update Config.c

--- Config.c
+++ Config.c
@@ -4,6 +4,7 @@
 #include "ValidateBattery.h"
 #include "SendAlertInfo.h"
 
+// Note : In future if more number of cooling types with varying temperatures are introduced, this configuration alone can be extended to meet the need.
 BatteryParam_st BatteryParamValues[MAX_COOLING_TYPES] =
 {
   {
@@ -38,6 +39,7 @@ AlertTarget_st AlertTargetInfo[MAX_ALERT_TARGET_POSSIBILITIES] =
 };
 
 
+// Note : In future if mail notification needs to be configured exclusively for a particular breach pattern, this can be configured here.
 MailNotification_st MailNotificationInfo[MAX_BREACH_TYPES] =
 {
   {
@@ -56,6 +58,8 @@ MailNotification_st MailNotificationInfo[MAX_BREACH_TYPES] =
 char AlertMessageOverEmail[MAX_BREACH_TYPES][100] =
 {"Not Applicable","Hi, the temperature is too low\n","Hi, the temperature is too high\n"};
 
+//In case of production environment following print functionalities are used.
+// Refer test-alerts.cpp file for the stub functions used in the unit test environment
 #ifdef PRODUCTION_ENVIRONMENT
 FnPtrPrintToController FuncPointerPrintToController = &PrintToController ;
 FnPtrPrintToEmail FuncPointerPrintToEmail = &PrintToEmail ;
