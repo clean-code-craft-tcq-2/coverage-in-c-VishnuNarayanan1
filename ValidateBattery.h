0cbf7ac047cf477533d09b44ab3bceb81cfa7b91
VishnuNarayanan1 <vishnu92narayanan@gmail.com>
2022-02-20 16:53:47 +0530

    Update ValidateBattery.h

--- ValidateBattery.h
+++ ValidateBattery.h
@@ -1,11 +1,6 @@
 #pragma once
 
 BreachType InferBreach(double Value, BatteryParam_st BatteryLimits);
-bool AlertBreach(AlertTarget AlertTarget, BreachType ProcessedBreachType);
-void SendAlertToController(BreachType breachType);
-void SendAlertToEmail(BreachType breachType);
 BatteryParam_st ClassifyTemp(CoolingType CoolingTypeInfo);
 bool ValidateBattery(AlertTarget AlertTargetIndex, CoolingType CoolingTypeInfo, double TemperatureInC);
 bool ValidateRange(size_t Var1, size_t Var2);
-void PrintToController (const unsigned short Header, BreachType BreachTypeInfo);
-void PrintToEmail (const char* Recepient, char Message[]);
